02bd4b3654397a9c5e661a1e8d42b2b928796103
Mujahid1717 <mujahidakhtar2003@gmail.com>
2023-02-17 16:42:18 +0500

    objects of array to display in the cards

--- App.tsx
+++ App.tsx
@@ -1,118 +1,138 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import React from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Home = () => {
+  const products = [
+    {
+      id: 1,
+      title: "Product 1",
+      category: "electronics",
+      price: 4000,
+      description: "This is description about product 1",
+      imageUrl:'https://picsum.photos/id/237/200/300',
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    },
+    {
+      id: 2,
+      title: "Product 2",
+      category: "electronics",
+      price: 2000,
+      description: "This is description about product 2",
+      imageUrl:'https://picsum.photos/200',
+    },
+    {
+      id: 3,
+      title: "Product 3",
+      category: "books",
+      price: 1000,
+      description: "This is description about product 3",
+      imageUrl:'https://picsum.photos/id/237/200/300',
+    },
+    {
+      id: 4,
+      title: "Product 4",
+      category: "fitness",
+      price: 3000,
+      description: "This is description about product 4",
+      imageUrl:'https://picsum.photos/200',
+    },
+    {
+      id: 5,
+      title: "Product 5",
+      category: "fitness",
+      price: 4000,
+      description: "This is description about product 5",
+      imageUrl:'https://picsum.photos/id/237/200/300',
+    },
+    {
+      id: 6,
+      title: "Product 6",
+      category: "gardening",
+      price: 5000,
+      description: "This is description about product 6",
+      imageUrl:'https://picsum.photos/200',
+    },
+    {
+      id: 7,
+      title: "Product 7",
+      category: "furniture",
+      price: 6000,
+      description: "This is description about product 7",
+      imageUrl:'https://picsum.photos/200/300',
+    },
+    {
+      id: 8,
+      title: "Product 8",
+      category: "furniture",
+      price: 7000,
+      description: "This is description about product 8",
+      imageUrl:'https://picsum.photos/200',
+    },
+    {
+      id: 9,
+      title: "Product 9",
+      category: "accessories",
+      price: 4000,
+      description: "This is description about product 9",
+      imageUrl:'https://picsum.photos/200/300',
+    },
+    {
+      id: 10,
+      title: "Product 10",
+      category: "electronics",
+      price: 3000,
+      description: "This is description about product 10",
+      imageUrl:'https://picsum.photos/200',
+     
+    }
+  
+  ];
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={styles.headerText}>Home</Text>
+      <ScrollView
+      style ={styles.container}
+      scrollEnabled = {true}
+      >
+      { products.map(({id, title, category,price, description,imageUrl})=>(
+        <View key = {id} style={styles.userCard}>
+          <Image
+            source={{uri:imageUrl}}
+            style = {styles.imageCard}
+          />
+          <Text>{title}</Text>
+          <Text>{category}</Text>
+          <Text>{price}</Text>
+          <Text>{description}</Text>
+
+        </View>
+      ))}
+    
+
+      </ScrollView>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  headerText:{
+    fontSize:26,
+    fontWeight:'bold',
+    marginHorizontal:10,
+    marginVertical:10
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container:{
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  userCard:{
+    alignItems:'center'
   },
-  highlight: {
-    fontWeight: '700',
+  imageCard:{
+    height:200,
+    width:200,
+    borderRadius: 200 / 2
   },
-});
 
-export default App;
+
+
+});
+export default Home;
